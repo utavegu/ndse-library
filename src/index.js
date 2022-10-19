e8cdf39e176d1c4b8e889077a8316651b0a01e00
Utavegu <utavegu@list.ru>
2022-10-19 13:34:28 +0500

    feat: add logger

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const logger = require('./middleware/logger');
 const error404 = require('./middleware/404');
 const authRouting = require('./routes/auth');
 const booksRouting = require('./routes/books');
 
 const app = express();
 app.use(express.json());
+
+app.use(logger);
 
 app.use('/api/user/login', authRouting);
 app.use('/api/books', booksRouting);
---
+++ src/middleware/logger.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const os = require('os');
+
+module.exports = (request, _, next) => {
+  const date = new Date().toISOString();
+  const { url, method } = request;
+
+  const data = `Date: ${date}\nMethod: ${method}\nEndpoint: ${url}\n`;
+
+  fs.appendFile("src/server.log", data + os.EOL, (error) => {
+    if (error) throw error;
+  });
+
+  next();
+};
