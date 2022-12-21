2d8ad811e4996c653d2b9a3dce1e86fe294588a0
Utavegu <utavegu@list.ru>
2022-12-21 13:49:53 +0500

    feat: add book interface, tsconfig and BooksRepository class

---
+++ library/src/typing/interfaces/Book.ts
@@ -0,0 +1,13 @@
+// Не понял как экспортировать
+
+interface Book {
+  title: string;
+  description: string;
+  authors: string;
+  favorite?: string;
+  fileCover?: string;
+  fileName?: string;
+  fileBook?: string;
+  counter?: string;
+  feedbacks?: string[];
+}
